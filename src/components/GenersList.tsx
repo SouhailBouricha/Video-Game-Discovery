98e9aba44c0446ea7bd75aae2f8f5c7122ea2efc
souhail bouricha <souhailbourigha@gmail.com>
2023-05-07 17:14:29 +0100

    Clean Up The Genres

--- src/components/GenersList.tsx
+++ src/components/GenersList.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Card, CardBody, HStack, Image, List, ListItem, Skeleton, SkeletonText, Spinner, Text } from "@chakra-ui/react";
+import { Box, Button, Card, CardBody, HStack, Heading, Image, List, ListItem, Skeleton, SkeletonText, Spinner, Text } from "@chakra-ui/react";
 import useGeners from "../hook/useGeners"
 import SkeletonCard from "./SkeletonCard";
 import { Genre } from '../hook/useGeners';
@@ -24,14 +24,17 @@ function GenersList({ updateGener,SelectedGener } : Props) {
         )
     }
     return (
-        <List>
-           {data.map(gener => <ListItem key={gener.id} paddingY={"5px"}>
-            <HStack>
-                <Image src={gener.image_background} boxSize={"32px"} borderRadius={8}/> 
-                <Button fontWeight={ gener.id === SelectedGener?.id ? "bold" : "normal"} onClick={() => updateGener(gener)} variant={"link"} fontSize={"lg"}>{gener.name}</Button>
-            </HStack>
-            </ListItem>)}
-        </List>
+        <>
+            <Heading fontSize={"2xl"} marginBottom={3}>Genres</Heading>
+            <List>
+               {data.map(gener => <ListItem key={gener.id} paddingY={"5px"}>
+                <HStack>
+                    <Image src={gener.image_background} objectFit={"cover"} boxSize={"32px"} borderRadius={8}/> 
+                    <Button whiteSpace={'normal'} textAlign={"left"} fontWeight={ gener.id === SelectedGener?.id ? "bold" : "normal"} onClick={() => updateGener(gener)} variant={"link"} fontSize={"lg"}>{gener.name}</Button>
+                </HStack>
+                </ListItem>)}
+            </List>
+        </>
     )
 }
 
