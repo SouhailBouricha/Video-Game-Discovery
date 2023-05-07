bdebc97cb858634902c0f7119a632f3754277a12
souhail bouricha <souhailbourigha@gmail.com>
2023-05-07 15:52:40 +0100

    Handle games with no image

--- src/services/Image-Url.ts
+++ src/services/Image-Url.ts
@@ -1,10 +1,12 @@
+import NoImage from "../assets/NoImage.webp";
+
 const GetOptimizeUrl = (url : string) =>{
     if(url){
         const index = url.indexOf('media/') + "media/".length;
         return url.slice(0,index) + "crop/600/400/" + url.slice(index);
     }
     else{
-        return url;
+        return NoImage;
     }
 };
 
